e2253d52e4f9b95e64d285628ebee1e91129cc19
wangluyao <wangluyao959277@163.com>
2022-02-12 12:45:02 +0800

    feat:保存

--- src/components/container/src/chooseIcon/index.vue
+++ src/components/container/src/chooseIcon/index.vue
@@ -3,12 +3,33 @@
 		<el-button type="primary" @click="handleClick">
 			<slot>选择图标</slot>
 		</el-button>
-		<el-dialog v-model="dialogVisible" :title="title" width="30%">
-			<slot></slot>
+		<el-dialog v-model="dialogVisible" :title="title" width="50%">
+			<el-scrollbar height="500px">
+				<div class="container">
+					<div
+						v-for="(item, index) in Object.keys(ElIcons)"
+						:key="index"
+						@click="clickItem(item)"
+					>
+						<div>
+							<!-- 动态组件 -->
+							<component
+								:is="`el-icon-${toLine(item)}`"
+							></component>
+						</div>
+						<div>
+							{{ item }}
+						</div>
+					</div>
+				</div>
+			</el-scrollbar>
 		</el-dialog>
 	</div>
 </template>
 <script setup lang="ts">
+import * as ElIcons from "@element-plus/icons-vue";
+import { toLine } from "./../../../../utils/index";
+import { useCopy } from "../../../../hooks/useCopy/index";
 import { watch, ref } from "vue";
 
 let props = defineProps<{
@@ -20,8 +41,16 @@ let emits = defineEmits(["update:visible"]);
 // 拷贝一份父组件传入的visible
 let dialogVisible = ref<boolean>(props.visible);
 
+// 点击弹出弹框
 const handleClick = () => {
 	emits("update:visible", true);
+};
+
+// 点击icon复制
+const clickItem = (item: string) => {
+	let text = `<el-icon-${toLine(item)}/>`;
+	useCopy(text); // 复制文本
+	dialogVisible.value = false
 };
 
 watch(
@@ -40,4 +69,20 @@ watch(
 	}
 );
 </script>
-<style lang="sass" scoped></style>
+<style lang="sass" scoped>
+
+.container
+	display: flex
+
+	align-items: center
+	justify-content: flex-start
+	flex-wrap: wrap
+	&>div
+		flex: 1 1 20%
+		text-align: center
+		margin-bottom: 1.5em
+		cursor: pointer
+		svg
+			width: 1.5em
+			height: 1.5em
+</style>
---
+++ src/hooks/useCopy/index.ts
@@ -0,0 +1,13 @@
+
+import { ElMessage } from "element-plus"
+
+// 复制文本
+export const useCopy = (text: string) => {
+	let input = document.createElement("input");
+	input.value = text;
+	document.body.appendChild(input);
+	input.select();
+	document.execCommand("copy");
+    document.body.removeChild(input);
+    ElMessage.success('复制成功')
+};
